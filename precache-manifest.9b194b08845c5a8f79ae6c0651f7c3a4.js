dc54f0b7d045960657f4d552db59b067df9a0063
ievlks <ieva.lekstutyte@telesoftas.com>
2020-09-20 23:39:37 +0300

    Updates

---
+++ precache-manifest.9b194b08845c5a8f79ae6c0651f7c3a4.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9daa72cf92153f60f2ffd5add00beb5",
+    "url": "http://localhost:3000/index.html"
+  },
+  {
+    "revision": "184fa086dc0bf05e1de5",
+    "url": "http://localhost:3000/static/js/2.01388019.chunk.js"
+  },
+  {
+    "revision": "23e75ecee431f5dfb96fa2deb80f243b",
+    "url": "http://localhost:3000/static/js/2.01388019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea03ded0cc6ce0c4970a",
+    "url": "http://localhost:3000/static/js/main.6a45d64b.chunk.js"
+  },
+  {
+    "revision": "eee3657e8501ae939433",
+    "url": "http://localhost:3000/static/js/runtime-main.716d304a.js"
+  },
+  {
+    "revision": "03499e1e61194907f505a5b4e73ad56b",
+    "url": "http://localhost:3000/static/media/back.03499e1e.svg"
+  },
+  {
+    "revision": "093a56b292bf0d2789d058d28021a257",
+    "url": "http://localhost:3000/static/media/clenching-fist-foto.093a56b2.jpg"
+  },
+  {
+    "revision": "ffce5508d70b1a8292b00c6c1cd61ae7",
+    "url": "http://localhost:3000/static/media/clenching-fist.ffce5508.mp4"
+  },
+  {
+    "revision": "5ff6618eab0cfae99de25cde1b2e7e17",
+    "url": "http://localhost:3000/static/media/dart-throwing-foto.5ff6618e.jpg"
+  },
+  {
+    "revision": "39fcbbbbd970be98caa755e9b0feced1",
+    "url": "http://localhost:3000/static/media/dart-throwing.39fcbbbb.mp4"
+  },
+  {
+    "revision": "0bc186f9621b3cfc85eab617b7312a3f",
+    "url": "http://localhost:3000/static/media/foot.0bc186f9.png"
+  },
+  {
+    "revision": "cf646e0c12da35a4969611e589a94c23",
+    "url": "http://localhost:3000/static/media/foot.cf646e0c.mp4"
+  },
+  {
+    "revision": "7c3071245df4170d47d3219c399a3668",
+    "url": "http://localhost:3000/static/media/hand.7c307124.png"
+  },
+  {
+    "revision": "1518b81d85dd34b3bed93303b6bf74b9",
+    "url": "http://localhost:3000/static/media/hip.1518b81d.jpg"
+  },
+  {
+    "revision": "65ae67e6419ca0d78773b14eec6433e3",
+    "url": "http://localhost:3000/static/media/hip.65ae67e6.mp4"
+  },
+  {
+    "revision": "cfe16f882145d78e2da12fd847c32c07",
+    "url": "http://localhost:3000/static/media/knee.cfe16f88.mp4"
+  },
+  {
+    "revision": "79c12c9f624a6ebb3ca4000a58c7a703",
+    "url": "http://localhost:3000/static/media/prono-supination.79c12c9f.mp4"
+  },
+  {
+    "revision": "f9bfc4f436c0e4507c8926976336db9d",
+    "url": "http://localhost:3000/static/media/radioulnar-deviation-foto.f9bfc4f4.jpg"
+  },
+  {
+    "revision": "3380f140ff78d132e7baab4f8327002a",
+    "url": "http://localhost:3000/static/media/radioulnar-deviation.3380f140.mp4"
+  },
+  {
+    "revision": "b37cbba90bb1b8611b85e230a2fc9920",
+    "url": "http://localhost:3000/static/media/scapulothoracic-joint-round-foto.b37cbba9.jpg"
+  },
+  {
+    "revision": "47db9db3ceb5aab79c34c4713710ba9f",
+    "url": "http://localhost:3000/static/media/scapulothoracic-joint-round.47db9db3.mp4"
+  },
+  {
+    "revision": "5380242e7862a8c88c8b0a4d77a70c5d",
+    "url": "http://localhost:3000/static/media/scapulothoracic-joint-up-foto.5380242e.jpg"
+  },
+  {
+    "revision": "e4e9cb76a612ec67f5fd415ed21e01ed",
+    "url": "http://localhost:3000/static/media/scapulothoracic-joint-up.e4e9cb76.mp4"
+  },
+  {
+    "revision": "1aff6507489c591c69fb35244ef915ea",
+    "url": "http://localhost:3000/static/media/shoulder.1aff6507.jpg"
+  }
+]);
