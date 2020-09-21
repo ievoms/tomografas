d7ce50dc1de9824781fc5f1efd828e67f37dd60c
ievlks <ieva.lekstutyte@telesoftas.com>
2020-09-21 12:45:32 +0300

    config

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,13 +6,10 @@ export const config: Config = {
   APP_URL:
     process.env.REACT_APP_APP_URL ||
     process.env.PUBLIC_URL ||
-    'http://localhost:3000/react-starter',
+    'http://localhost:3000',
   API_URL:
     process.env.REACT_APP_API_URL ||
     process.env.API_URL ||
     'https://cat-fact.herokuapp.com/',
-  BASE_URL:
-    process.env.REACT_APP_BASE_URL ||
-    process.env.PUBLIC_URL ||
-    '/react-starter',
+  BASE_URL: process.env.REACT_APP_BASE_URL || process.env.PUBLIC_URL || '/',
 }
